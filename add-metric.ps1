e7693e06ea69e8803a6dba3f53414eb2126f7a3c
Scott Drummonds <orcsab@gmail.com>
2020-06-22 10:16:50 +0800

    Automatically generating timestamp.

--- add-metric.ps1
+++ add-metric.ps1
@@ -1,28 +1,35 @@
 
 $cred = Get-Credential
 
+$date = Get-Date
+$timestamp = $date.ToUniversalTime().toString('yyyy-MM-ddTHH:mm:ss')
+
+$metric = 6.89
+
 $record = @{
   seriesRef = @{
-    subject = "D867efbcf1bcd1c10a000fd1f0a4bcb92"
+    subject = "71a872a51b6990107d7bedf32a4bcbff"
     table = "u_fridge"
     metric = "u_temp"
   }
   values = @(
     @{
-    timestamp = "2020-06-01T00:00:00Z"
-    value = 0.0
-    },
-    @{
-    timestamp = "2020-06-01T00:01:00Z"
-    value = 1.1
-    },
-    @{
-    timestamp = "2020-06-01T00:02:00Z"
-    value = 2.2
+    timestamp = $timestamp
+    value = $metric
     }
   )
 }
 
 $bodyJson = $record | ConvertTo-Json
 
-$response = Invoke-WebRequest -Uri https://drummonds.service-now.com/api/now/v1/clotho/put?align_time -Credential $cred -Method Post -Body $bodyJson -ContentType "application/json"
+$response = Invoke-WebRequest `
+  -Uri https://drummonds.service-now.com/api/now/v1/clotho/put `
+  -Credential $cred `
+  -Method Post `
+  -Body $bodyJson `
+  -ContentType "application/json" `
+  -Headers @{'Accept' = 'application/json'}
+
+"Timestamp = " + $timestamp
+"Data = " + $metric
+$response
